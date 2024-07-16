9b4e67ed203abaf671f0230829886240dc83d688
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-07-16 15:20:50 +0200

    own file

---
+++ BeautifulServerApplication/SetupConfig.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace BeautifulServerApplication;
+
+public class SetupConfig
+{
+	[ModuleInitializer]
+	public static void Init()
+	{
+		Initialize();
+	}
+
+	public static void Initialize()
+	{
+		var currentDirectory = Directory.GetCurrentDirectory();
+		var basePath = Directory.GetParent(currentDirectory)?.Parent?.Parent?.ToString();
+
+		try
+		{
+			if (!string.IsNullOrEmpty(basePath))
+			{
+				new ConfigurationBuilder()
+					.SetBasePath(basePath)
+					.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+					.AddEnvironmentVariables()
+					.Build();
+			}
+		}
+		catch (ArgumentException argumentException)
+		{
+			Log.Error($"Wrong basePath: {basePath}\n" +
+			          $"[{argumentException.ParamName}]: {argumentException.Message}" +
+			          " ||{SessionKey}||", "server");
+		}
+		catch (FileNotFoundException fileNotFoundException)
+		{
+			Log.Error($"File not found: {fileNotFoundException.FileName}\n" +
+			          $"[{fileNotFoundException.GetType()}]: {fileNotFoundException.Message}" +
+			          " ||{SessionKey}||", "server");
+		}
+		catch (Exception ex)
+		{
+			Log.Fatal("!!! Unexpected error\n" +
+			          "Base path is null or empty. Cannot load configuration." +
+			          $"{ex.Message}" +
+			          " ||{SessionKey}||", "server");
+		}
+	}
+}
--- BeautifulServerApplication/SetupLogging.cs
+++ BeautifulServerApplication/SetupLogging.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-using Microsoft.Extensions.Configuration;
 using Serilog;
 
 namespace BeautifulServerApplication
@@ -28,52 +27,6 @@ namespace BeautifulServerApplication
 				.WriteTo.Console(outputTemplate:
 					outputTemplate)
 				.CreateLogger();
-		}
-	}
-
-	public class SetupConfig
-	{
-		[ModuleInitializer]
-		public static void Init()
-		{
-			Initialize();
-		}
-
-		public static void Initialize()
-		{
-			var currentDirectory = Directory.GetCurrentDirectory();
-			var basePath = Directory.GetParent(currentDirectory)?.Parent?.Parent?.ToString();
-
-			try
-			{
-				if (!string.IsNullOrEmpty(basePath))
-				{
-					new ConfigurationBuilder()
-						.SetBasePath(basePath)
-						.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-						.AddEnvironmentVariables()
-						.Build();
-				}
-			}
-			catch (ArgumentException argumentException)
-			{
-				Log.Error($"Wrong basePath: {basePath}\n" +
-				          $"[{argumentException.ParamName}]: {argumentException.Message}" +
-				          " ||{SessionKey}||", "server");
-			}
-			catch (FileNotFoundException fileNotFoundException)
-			{
-				Log.Error($"File not found: {fileNotFoundException.FileName}\n" +
-				          $"[{fileNotFoundException.GetType()}]: {fileNotFoundException.Message}" +
-				          " ||{SessionKey}||", "server");
-			}
-			catch (Exception ex)
-			{
-				Log.Fatal("!!! Unexpected error\n" +
-				          "Base path is null or empty. Cannot load configuration." +
-				          $"{ex.Message}" +
-				          " ||{SessionKey}||", "server");
-			}
 		}
 	}
 }
