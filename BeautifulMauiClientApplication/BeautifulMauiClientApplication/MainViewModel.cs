fcabc36bf089ae71c49ace33b813eaefd8a1a5bc
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-08-03 11:41:14 +0200

    Adding community toolkit / mvvm example for understanding

---
+++ BeautifulMauiClientApplication/BeautifulMauiClientApplication/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BeautifulMauiClientApplication
+{
+	public partial class MainViewModel : ObservableObject
+	{
+		private readonly IDataService _dataService;
+
+		[ObservableProperty] private ObservableCollection<ItemObj> _items;
+
+		[ObservableProperty] [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+		private bool _isBusy;
+
+		public bool IsNotBusy => !IsBusy;
+
+		public MainViewModel(IDataService dataService)
+		{
+			_dataService = dataService;
+			Items = new ObservableCollection<ItemObj>();
+		}
+
+		[RelayCommand]
+		private async Task LoadItemsAsync()
+		{
+			if (IsBusy)
+				return;
+
+			try
+			{
+				IsBusy = true;
+				var items = await _dataService.GetItemsAsync();
+				Items.Clear();
+				foreach (var item in items)
+				{
+					Items.Add(item);
+				}
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+	}
+
+	public interface IDataService
+	{
+		Task<List<ItemObj>> GetItemsAsync();
+	}
+
+	public class DataService : IDataService
+	{
+		public async Task<List<ItemObj>> GetItemsAsync()
+		{
+			await Task.Delay(1000);
+			return new List<ItemObj>
+			{
+				new ItemObj { Id = 1, Name = "ItemObj 1" },
+				new ItemObj { Id = 2, Name = "ItemObj 2" },
+				new ItemObj { Id = 3, Name = "ItemObj 3" }
+			};
+		}
+	}
+
+	public class ItemObj
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
--- BeautifulMauiClientApplication/BeautifulMauiClientApplication/MauiProgram.cs
+++ BeautifulMauiClientApplication/BeautifulMauiClientApplication/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace BeautifulMauiClientApplication
 {
@@ -9,6 +10,7 @@ namespace BeautifulMauiClientApplication
 			var builder = MauiApp.CreateBuilder();
 			builder
 				.UseMauiApp<App>()
+				.UseMauiCommunityToolkit()
 				.ConfigureFonts(fonts =>
 				{
 					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -18,6 +20,10 @@ namespace BeautifulMauiClientApplication
 #if DEBUG
 			builder.Logging.AddDebug();
 #endif
+			builder.Services.AddSingleton<IDataService, DataService>();
+
+			builder.Services.AddTransient<MainPage>();
+			builder.Services.AddTransient<MainViewModel>();
 
 			return builder.Build();
 		}
