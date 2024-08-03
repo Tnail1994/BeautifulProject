406e2c50613c2f8bfd95a2046e10172659211e62
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-08-03 10:21:31 +0200

    Created a maui client application (initial)

---
+++ BeautifulMauiClientApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BeautifulMauiClientApplication
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ BeautifulMauiClientApplication/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BeautifulMauiClientApplication
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
